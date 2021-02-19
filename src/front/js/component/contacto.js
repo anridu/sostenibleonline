7e7ee5eddb83d5fca2bebd160e757bec0c59749e
soniascr <soniascr@gmail.com>
2021-02-19 20:17:10 +0000

    add Google Map to homepage

--- src/front/js/component/contacto.js
+++ src/front/js/component/contacto.js
@@ -1,15 +1,35 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.scss";
-import { StaticGoogleMap, Marker, Path } from "react-static-google-map";
+import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+	iconUrl: icon,
+	shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 export const Contact = () => {
 	return (
 		<div className="row">
-			<div className="col-md-6">
-				<StaticGoogleMap size="600x600" className="img-fluid" apiKey="AIzaSyCH3vx4xaFNDN46a2OfJBov-QKdK69hMmQ">
-					<Marker location="6.4488387,3.5496361" color="blue" label="P" />
-				</StaticGoogleMap>
+			<div className="col-md-6 p-3 ">
+				<MapContainer center={[41.3818, 2.1685]} zoom={13} scrollWheelZoom={false} style={{ height: 300 }}>
+					<TileLayer
+						attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					/>
+					<Marker position={[41.3818, 2.1685]}>
+						<Popup>
+							A pretty CSS3 popup. <br /> Easily customizable.
+						</Popup>
+					</Marker>
+				</MapContainer>
 			</div>
 			<div className="col-md-6 py-3">
 				<h4>Contacta con nosotros</h4>
