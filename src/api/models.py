4b0b9346e022e1e2c07f68ca78b7ff43a4065c24
soniascr <soniascr@gmail.com>
2021-02-24 20:12:45 +0000

    add product database

--- src/api/models.py
+++ src/api/models.py
@@ -52,3 +52,29 @@ class Business(db.Model):
               "id": self.id,
               "name": self.name
             }
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(120), unique=True, nullable=False)
+    web = db.Column(db.String(80), unique=False, nullable=False)
+    quantity = db.Column(db.Integer, unique=False, nullable=False)
+    size = db.Column(db.String(6), unique=False, nullable=False)  
+    description = db.Column(db.String, unique=False, nullable=True)
+    check = db.Column(db.Boolean(), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<Product %r>' % self.web
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "product_name": self.product_name,
+            "web": self.web,
+            "quantity": self.quantity,
+            "size": self.size,
+            "description": self.description,
+            "check": self.check,
+            # do not serialize the password, its a security breach
+        }
+
+    def get_all_products():
+        return Product.query.all()
