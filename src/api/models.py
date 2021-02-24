4d82648ac9fc189bb346becf321646e8e6cc9950
enaguero <enaguero@gmail.com>
2021-02-24 19:52:11 +0000

    First version for database model

--- src/api/models.py
+++ src/api/models.py
@@ -2,11 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    users = db.relationship("User", secondary="designation")
+
+    def __repr__(self):
+        return f'<Role {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+          }
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    roles = db.relationship("Role", secondary="designation")
+    businesses = db.relationship('Business', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +33,22 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Designation(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+class Business(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Business {self.name}>'
+
+    def serialize(self):
+        return {
+              "id": self.id,
+              "name": self.name
+            }
