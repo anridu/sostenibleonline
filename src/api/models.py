9b034176d40e3bc04b26e5f3b29e75d9ea242ad5
SamHnnz <hlsamuel0@gmail.com>
2021-02-25 19:04:17 +0000

    DB relationships v1

--- src/api/models.py
+++ src/api/models.py
@@ -2,11 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    users = db.relationship("User", secondary="designation")
+
+    def __repr__(self):
+        return f'<Role {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+          }
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    businesses = db.relationship('Business', backref='user', lazy=True)
+    roles = db.relationship("Role", secondary="designation")
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -17,32 +33,24 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
-        
 
-class Product(db.Model):
+class Designation(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+  role = db.relationship("Role")
+  user = db.relationship("User")
+
+class Business(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    product_name = db.Column(db.String(120), unique=True, nullable=False)
-    web = db.Column(db.String(80), unique=False, nullable=False)
-    quantity = db.Column(db.Integer, unique=False, nullable=False)
-    size = db.Column(db.String(6), unique=False, nullable=False)  
-    description = db.Column(db.String, unique=False, nullable=True)
-    check = db.Column(db.Boolean(), unique=False, nullable=True)
-     
+    name = db.Column(db.String(80), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return '<Product %r>' % self.web
+        return f'<Business {self.name}>'
 
     def serialize(self):
         return {
-            "id": self.id,
-            "product_name": self.product_name,
-            "web": self.web,
-            "quantity": self.quantity,
-            "size": self.size,
-            "description": self.description,
-            "check": self.check,
-            # do not serialize the password, its a security breach
-        }
-
-    def get_all_products():
-        return Product.query.all()
+              "id": self.id,
+              "name": self.name
+            }
