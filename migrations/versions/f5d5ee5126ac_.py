86079c10ce21112aec76c22bd1c9937a36f378db
anridu <angelriverod@gmail.com>
2021-03-29 16:48:42 +0000

    data base

--- migrations/versions/f5d5ee5126ac_.py
+++ migrations/versions/f5d5ee5126ac_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 78a95b770936
+Revision ID: f5d5ee5126ac
 Revises: 
-Create Date: 2021-03-26 18:20:47.286992
+Create Date: 2021-03-29 16:47:17.320688
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '78a95b770936'
+revision = 'f5d5ee5126ac'
 down_revision = None
 branch_labels = None
 depends_on = None
