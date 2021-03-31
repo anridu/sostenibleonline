a9acdb70ca778dfe6268721bd6b3abddd5d20429
soniascr <soniascr@gmail.com>
2021-03-31 19:37:17 +0000

    first version of product page

--- src/front/js/pages/producto.js
+++ src/front/js/pages/producto.js
@@ -1,22 +1,24 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import Context from "../store/appContext";
+import { apiBaseURL } from "../constants";
+import PropTypes from "prop-types";
 //include images into your bundle
 import marca from "../../img/ropa.jpg";
 
 //create your first component
-export function Product() {
+export const Product = data => {
+	const params = useParams();
+	const { product_name, description, price } = data.data;
 	return (
 		<div>
 			<div>
 				<div className="card mb-3">
 					<div className="row g-0">
-						<div className="col-md-4">
-							<img src={marca} className="img-thumbnail" />{" "}
-						</div>
+						<div className="col-md-4">{/* <img src={data ? imageUrl : "loading"} /> */}</div>
 						<div className="col-md-8">
 							<div className="card-body">
-								<h5 className="card-title">Título del producto</h5>
-								<h5 className="card-title">Precio</h5>
+								<h5 className="card-title">{data ? product_name : "loading"}</h5>
 
 								<div className="dropdown">
 									<button
@@ -71,6 +73,10 @@ export function Product() {
 										</a>
 									</div>
 								</div>
+								<h5 className="card-title">
+									{data ? price : "loading"}
+									euros
+								</h5>
 							</div>
 						</div>
 					</div>
@@ -87,24 +93,18 @@ export function Product() {
 										Descripción
 									</a>
 								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="#">
-										Información adicional
-									</a>
-								</li>
 							</ul>
 						</div>
 						<div className="card-body">
-							<h4 className="card-title">Titulo de la tarjeta</h4>
-							<p className="card-text">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lectus sem, tempor vitae
-								mattis malesuada, ornare sed erat. Pellentesque nulla dui, congue nec tortor sit amet,
-								maximus mattis dui.{" "}
-							</p>
+							<p className="card-text">{data ? description : "loading"}</p>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	);
-}
+};
+
+Product.propTypes = {
+	data: PropTypes.object
+};
